507413e1629432eefc52388b7a8b61c2720fdca9
Tejeshwar Reddy <tejeshwarreddy25@gmail.com>
2024-03-03 09:51:50 +0530

    Modified doc stringa

--- src/health/routers.py
+++ src/health/routers.py
@@ -20,10 +20,7 @@ router = APIRouter(
 def get_health() -> HealthCheck:
     """
     ## Perform a Health Check
-    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
-    to ensure a robust container orchestration and management is in place. Other
-    services which rely on proper functioning of the API service will not deploy if this
-    endpoint returns any other HTTP status code except 200 (OK).
+    Endpoint to perform a healthcheck on. 
     Returns:
         HealthCheck: Returns a JSON response with the health status
     """
